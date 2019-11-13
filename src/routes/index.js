65b1201aebd4401e335339427de0c0db59ed8891
JuanFer <junfer.1105@gmail.com>
2019-11-13 11:39:02 -0500

    Se corrigen las rutas, para que se acceda al home para las pruebas

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hola que hace');
+    res.render('links/home');
 })
 
 module.exports = router;
